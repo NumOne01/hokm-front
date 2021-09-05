05286ea6d5915f71dde91395c97af241fec39d88
alireza.fz96 <alireza.fz96@gmail.com>
2021-09-05 17:53:10 +0430

    fixed cards

--- src/App.tsx
+++ src/App.tsx
@@ -6,26 +6,23 @@ import styles from './styles.module.css';
 
 const cards: string[] = [];
 
-for (let i = 0; i < 52; i++)
-	cards.push(
-		'https://upload.wikimedia.org/wikipedia/en/f/f5/RWS_Tarot_08_Strength.jpg'
-	);
+const KHALS = ['pike', 'heart', 'clover', 'tile'];
 
-// const cards = [
-// 	'https://upload.wikimedia.org/wikipedia/en/f/f5/RWS_Tarot_08_Strength.jpg',
-// 	'https://upload.wikimedia.org/wikipedia/en/5/53/RWS_Tarot_16_Tower.jpg',
-// 	'https://upload.wikimedia.org/wikipedia/en/9/9b/RWS_Tarot_07_Chariot.jpg',
-// 	'https://upload.wikimedia.org/wikipedia/en/d/db/RWS_Tarot_06_Lovers.jpg',
-// 	'https://upload.wikimedia.org/wikipedia/en/thumb/8/88/RWS_Tarot_02_High_Priestess.jpg/690px-RWS_Tarot_02_High_Priestess.jpg',
-// 	'https://upload.wikimedia.org/wikipedia/en/d/de/RWS_Tarot_01_Magician.jpg',
-// 	'https://upload.wikimedia.org/wikipedia/en/d/de/RWS_Tarot_01_Magician.jpg',
-// 	'https://upload.wikimedia.org/wikipedia/en/d/de/RWS_Tarot_01_Magician.jpg',
-// 	'https://upload.wikimedia.org/wikipedia/en/d/de/RWS_Tarot_01_Magician.jpg',
-// 	'https://upload.wikimedia.org/wikipedia/en/d/de/RWS_Tarot_01_Magician.jpg',
-// 	'https://upload.wikimedia.org/wikipedia/en/d/de/RWS_Tarot_01_Magician.jpg',
-// 	'https://upload.wikimedia.org/wikipedia/en/d/de/RWS_Tarot_01_Magician.jpg',
-// 	'https://upload.wikimedia.org/wikipedia/en/d/de/RWS_Tarot_01_Magician.jpg'
-// ];
+const pictures = ['king', 'queen', 'soldier']
+
+for(const khal of KHALS) {
+	for(let i = 1; i < 11; i++) {
+		cards.push(
+			`/assets/cards/${khal}_${i}.svg`
+		);
+	}
+	
+	for(const picture of pictures) {
+		cards.push(
+			`/assets/cards/${khal}_${picture}.svg`
+		);
+	}
+}
 
 const TURN: number = 0;
 const CARDS_PER_PERSON: number = 13;
@@ -99,7 +96,7 @@ const trans = (r: number, s: number) =>
 	}deg) rotateZ(${r}deg) scale(${s})`;
 
 function Deck() {
-	const [gone] = useState(() => new Set()); // The set flags all the cards that are flicked out
+	const [gone, setGone] = useState(() => new Set()); // The set flags all the cards that are flicked out
 	const [props, api] = useSprings(cards.length, i => ({
 		to: to(i),
 		from: from(i)
@@ -107,28 +104,27 @@ function Deck() {
 
 	// Create a gesture, we're interested in down-state, delta (current-pos - click-pos), direction and velocity
 	const bind = useDrag(
-		({ args: [index], down, movement: [mx], direction: [xDir], velocity }) => {
+		({ args: [index], down, movement: [mx, my], direction: [xDir], velocity }) => {
 			const trigger = velocity > 0.2; // If you flick hard enough it should trigger the card to fly out
 			const dir = xDir < 0 ? -1 : 1; // Direction should either point left or right
-			if (!down && trigger) gone.add(index); // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
+			
+			if (down && trigger) gone.add(index);
 			api.start(i => {
 				if (index !== i) return; // We're only interested in changing spring-data for the current spring
 				const isGone = gone.has(index);
-				const x = 0; // When a card is gone it flys out left or right, otherwise goes back to zero
 				const rot = mx / 100 + (isGone ? dir * 10 * velocity : 0); // How much the card tilts, flicking it harder makes it rotate faster
 				const scale = down ? 1.1 : 1; // Active cards lift up a bit
+				const initialCooridinates = to(i)[0];
+
 				return {
-					x,
-					y: -40,
-					rot,
-					scale: 0.8,
-					delay: undefined,
-					config: { friction: 50, tension: down ? 800 : isGone ? 200 : 500 }
+					x: down ? mx : isGone ? 0 : initialCooridinates.x, 
+					y: down ? my : isGone ? -40 : initialCooridinates.y,
+					scale: scale,
 				};
 			});
 		}
-	);
-	// Now we're just mapping the animated values to our view, that's it. Btw, this component only renders once. :-)
+	, { initial: (comm) => [props[comm.args].x.get(), props[comm.args].y.get()], bounds: comm => !gone.has(props[comm.args]) && ({ left: props[comm.args].x.get(), right: props[comm.args].x.get() -200, top: props[comm.args].y.get() - 60, bottom: props[comm.args].y.get() + 20 })});
+
 	return (
 		<>
 			{props.map(({ x, y, rot, scale }, i) => (
